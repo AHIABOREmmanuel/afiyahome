916a5b0e3e6ffcac8435fc92b2005b0cab8135d4
Emmanuel ahiabor <ahiaboremmanuel9@gmail.com>
2025-06-03 10:56:14 +0000

    personnalisation du template

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './components/main/main.component';
+import { AboutComponent } from './pages/about/about.component';
+import { MenComponent } from './pages/men/men.component';
+import { WomenComponent } from './pages/women/women.component';
+import { AccessoriesComponent } from './pages/accessories/accessories.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { CardComponent } from './pages/card/card.component';
+import { KidsComponent } from './pages/kids/kids.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component: MainComponent},
+  {path:"about",component: AboutComponent},
+  {path:"men",component: MenComponent},
+  {path:"women",component: WomenComponent},
+  {path:"kid",component: KidsComponent},
+  {path:"accessories",component: AccessoriesComponent},
+  {path:"contacts",component: ContactsComponent},
+  {path:"card",component: CardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,29 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './components/main/main.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { NavComponent } from './components/nav/nav.component';
+import { AboutComponent } from './pages/about/about.component';
+import { MenComponent } from './pages/men/men.component';
+import { WomenComponent } from './pages/women/women.component';
+import { KidsComponent } from './pages/kids/kids.component';
+import { AccessoriesComponent } from './pages/accessories/accessories.component';
+import { ContactsComponent } from './pages/contacts/contacts.component';
+import { CardComponent } from './pages/card/card.component';
+import { SubscribeComponent } from './pages/subscribe/subscribe.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    AboutComponent,
+    MenComponent,
+    WomenComponent,
+    KidsComponent,
+    AccessoriesComponent,
+    ContactsComponent,
+    CardComponent,
+    SubscribeComponent
   ],
   imports: [
     BrowserModule,
